51711db870fd37516e21a2581dc8e735ea81d0f5
hanseltime <hanseltime@gmail.com>
2025-02-12 10:47:02 -0700

    ci: hardening documentation pipeline

--- release.config.js
+++ release.config.js
@@ -35,11 +35,6 @@ module.exports = {
 		// ],
 		// // This creates a release on github - you can decide if you want to mirror the files in package.json
 		// "@semantic-release/github",
-		// Create documentation for the new version
-		{
-			path: "@semantic-release/exec",
-			cmd: "./bin/publish-docs.sh ${nextRelease.version}",
-		},
 	],
 	ci: false,
 };
